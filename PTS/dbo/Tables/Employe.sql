a339ecf8e6ae442235edbdf571b4e19cf4cf1b27
jeremy_woo <jeremy.woo@clearpoint.co.nz>
2018-01-04 07:58:13 +1300

    added pts database project.

---
+++ PTS/dbo/Tables/Employe.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Employe]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] VARCHAR(50) NULL, 
+    [Description] VARCHAR(200) NULL
+)
---
+++ PTS/dbo/Tables/User.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[User] (
+    [id]       INT          IDENTITY (1, 1) NOT NULL,
+    [name]     VARCHAR (50) NULL,
+    [username] VARCHAR (50) NULL,
+    [password] VARCHAR (50) NULL,
+    CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
