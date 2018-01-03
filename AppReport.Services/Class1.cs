1bbff30c6dfbb03949b760b34a3390b28319a467
jeremy_woo <jeremy.woo@clearpoint.co.nz>
2018-01-04 08:38:11 +1300

    Added databse project and entityframework.

---
+++ AppReport.Services/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AppReport.Services
+{
+    public class Class1
+    {
+    }
+}
---
+++ AppReport.Services/PTSDataModel/PTSContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace AppReport.Services.PTSDataModel
+{
+    public partial class PTSContext : DbContext
+    {
+        public virtual DbSet<User> User { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+            optionsBuilder.UseSqlServer(@"Server=localhost;Database=PTS;Trusted_Connection=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Name)
+                    .HasColumnName("name")
+                    .HasColumnType("varchar(50)");
+
+                entity.Property(e => e.Password)
+                    .HasColumnName("password")
+                    .HasColumnType("varchar(50)");
+
+                entity.Property(e => e.Username)
+                    .HasColumnName("username")
+                    .HasColumnType("varchar(50)");
+            });
+        }
+    }
+}
