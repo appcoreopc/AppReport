b71c4ee625c54148e94b8ef81e993b3d870c8f71
kepung <kepung@gmail.com>
2018-01-10 21:44:54 +1300

    effects / reducer major commit

--- WebUx/src/app/material/material-category-component/material-category-component.component.ts
+++ WebUx/src/app/material/material-category-component/material-category-component.component.ts
@@ -1,4 +1,11 @@
+
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { CityAppState, USER_GET, USER_GET_OK, USER_SAVE } from '../../sharedObjects/sharedMessages';
+import { Subscription } from 'rxjs/Subscription'
+import * as messageUtil from "../../sharedObjects/storeMessageUtil";
+import { MaterialCategoryModel } from "../../model/MaterialCategoryModel";
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-material-category-component',
@@ -7,9 +14,131 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MaterialCategoryComponentComponent implements OnInit {
 
-  constructor() { }
+  private person: MaterialCategoryModel = new MaterialCategoryModel();
+  private personForm: FormGroup;
 
-  ngOnInit() {
+  formErrors = {
+    'name': '',
+    'username': ''
+  };
+
+  validationMessages = {    
+    'name': {
+      'required': 'First Name is required.',
+      'minlength': 'First Name must be at least 4 characters long.',
+      'maxlength': 'First Name cannot be more than 24 characters long.'
+    },
+    'username': {
+      'required': 'Last Name is required.',
+      'minlength': 'Last Name must be at least 4 characters long.',
+      'maxlength': 'Last Name cannot be more than 24 characters long.'
+    }
+  };
+  
+  rows = [];
+
+  columns = [
+    { prop: 'name' },
+    { name: 'username' }   
+  ];
+
+  userSubscription : Subscription;
+  dataList : Array<any> = new Array<any>(); 
+
+  constructor(private store : Store<CityAppState>, 
+    private fb: FormBuilder) { 
   }
 
+  ngOnInit() {   
+    this.userSubscription = this.store.subscribe(appData => {           
+      this.componentMessageHandle(messageUtil.handleMessage(messageUtil.getMessage(appData, USER_GET_OK), USER_GET_OK));
+    }); 
+
+    this.initForm();
+  }
+
+  ngAfterViewInit() {
+     this.dispatchIntent(USER_GET);
+  }
+   
+  save() {    
+
+     var saveJson = {
+      Name : this.person.name,
+      Username : this.person.username
+    };
+
+    console.log(JSON.stringify(saveJson));
+    this.dispatchIntent(USER_SAVE, saveJson);
+    //this.personForm.reset();
+
+  }  
+
+  componentMessageHandle(message : any) {
+
+    if (message && message.type == USER_GET_OK)
+    {
+      this.rows.length = 0;
+      for (var idx in message.data)
+      {
+        var userInfo = message.data[idx];    
+        this.dataList.push({  
+            name : userInfo.name, 
+            username : userInfo.username
+        });
+
+        console.log(this.rows);
+        this.rows = this.dataList;
+      }
+    }    
+  }
+
+  private initForm() {
+    this.personForm = this.fb.group({
+      'name': [this.person.name, [Validators.required, Validators.minLength(1),
+      Validators.maxLength(24)]],
+      'username': [this.person.username, [Validators.required, Validators.minLength(1),
+      Validators.maxLength(24)]]
+    });
+
+    this.personForm.valueChanges.debounceTime(500)
+      .subscribe(data => this.onValueChanged(data));
+  }
+
+  onValueChanged(data?: MaterialCategoryModel) {
+
+    if (!this.personForm) { return; }
+
+    const form = this.personForm;
+    this.person.name = data.name;
+    this.person.username = data.username;
+
+    for (const field in this.formErrors) {
+      // clear previous error message (if any)
+      this.formErrors[field] = '';
+      const control = form.get(field);
+
+      if (control && control.dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+        for (const key in control.errors) {
+          this.formErrors[field] += messages[key] + ' ';
+        }
+      }
+    }   
+  }
+
+  onSubmit() {
+    
+    console.log(this.person.name);
+    console.log(this.person.username);
+  }
+
+  dispatchIntent(messageType : string, data? : any)
+  {   
+    this.store.dispatch(
+      {     
+        type: messageType,
+        data : data
+      });      
+  }
 }
