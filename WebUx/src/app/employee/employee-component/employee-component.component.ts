c840309bfa4c85a6192473ebfea63b8bf685b71c
jeremy_woo <jeremy.woo@clearpoint.co.nz>
2018-02-13 10:35:55 +1300

    added progress show and hide in employee screen.

--- WebUx/src/app/employee/employee-component/employee-component.component.ts
+++ WebUx/src/app/employee/employee-component/employee-component.component.ts
@@ -1,278 +1,264 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { CityAppState, EMPLOYEE_SAVE, EMPLOYEE_GET_OK,
+import {
+  CityAppState, EMPLOYEE_SAVE, EMPLOYEE_GET_OK,
   ADD, UPDATE, EMPLOYEE_GET, EMPLOYEE_SAVE_SUCCESS,
-JOBTITLE_GET, JOBTITLE_GET_OK } from '../../sharedObjects/sharedMessages';
+  JOBTITLE_GET, JOBTITLE_GET_OK
+} from '../../sharedObjects/sharedMessages';
 import { EmployeeModel } from "../../model/EmployeeModel";
-import { JobTitleModel } from "../../model/JobTitleModel"; 
+import { JobTitleModel } from "../../model/JobTitleModel";
 import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Subscription } from 'rxjs/Subscription'
 import * as messageUtil from "../../sharedObjects/storeMessageUtil";
-import {DialogModule} from 'primeng/dialog';
-  
-  @Component({  
-    selector: 'app-employee-component',
-    templateUrl: './employee-component.component.html',
-    styleUrls: ['./employee-component.component.css']
-  })
-  
-  export class EmployeeComponentComponent implements OnInit {
-    
-    private person: EmployeeModel = new EmployeeModel(); 
-    private personForm: FormGroup;
-    private intention : number = UPDATE;
-    
-    display: boolean = false;
-    
-    formTitle: string = "New Employee"; 
-    dataList : Array<any> = new Array<any>(); 
-    jobTitleDataList : Array<any> = new Array<any>(); 
-    
-    formErrors = {
-      'empName': '',
-      'empIdno': '',    
-      'empAd1': '',
-      'empAd2': '',
-      'empAd3': '',
-      'jobTitleId' : ''    
-    };
-    
-    itemSelected : boolean = false;
-    
-    validationMessages = {    
-      'empName': {
-        'required': 'Name is required.' 
-      },
-      'empIdno': {
-        'required': 'IC No. is required.' 
-      },
-      
-      'empAd1': {
-        'required': 'Address 1 is required.' 
-      },
-      'empAd2': {
-        'required': 'Address 2 is required.' 
-      },
-      'empAd3': {
-        'required': 'Address3 is required.' 
-      },
-      'jobTitleId': {
-        'required': 'Job Title is required.' 
-      }
-    };
-    
-    userSubscription : Subscription;
-    
-    rows = [];
-    jobTitleRows = [];
-    
-    columns = [
-      { prop: 'empName', name : 'Name' },
-      { prop: 'empIdno', name : 'Employee No' },    
-      { prop: 'empAd1', name : 'Address', width : 350 },
-      { prop: 'empAd2', name : 'Address 2', width : 350 },   
-      { prop: 'empAd3', name : 'Address 3', width : 350 }   
-    ];
-    
-    constructor(private store : Store<CityAppState>, private fb: FormBuilder) { }
-    
-    name : string; 
-    description : string; 
-    
-    ngOnInit() {
-      
-      this.userSubscription = this.store.subscribe(appData => { 
-        
-        this.componentMessageHandle(messageUtil.getMultiMessage(appData, 
-          [ EMPLOYEE_GET_OK, EMPLOYEE_SAVE_SUCCESS, JOBTITLE_GET_OK]));
-        }); 
-      
-        this.configureEditForm();
-      }
-      
-      ngAfterViewInit() {            
-        this.dispatchIntent(EMPLOYEE_GET);
-        this.dispatchIntent(JOBTITLE_GET);
-      }
-      
-      save()
-      {     
-        
-        var saveJson = new EmployeeModel();
-        if (this.intention == ADD)
-        {
-          saveJson = this.personForm.value as EmployeeModel;
-        }
-        else {
-          
-          saveJson.empId = this.person.empId;
-          saveJson.empName = this.person.empName;
-          saveJson.empIdno = this.person.empIdno;
-          saveJson.empAd1 = this.person.empAd1;
-          saveJson.empAd2 = this.person.empAd2;
-          saveJson.empAd3 = this.person.empAd3;
-          saveJson.jobTitleId = this.person.jobTitleId;
-        }
-        
-        var strJson = JSON.stringify(saveJson);           
-        this.dispatchIntent(EMPLOYEE_SAVE, saveJson);
-        this.display = false; 
-      } 
+import { DialogModule } from 'primeng/dialog';
 
-      private setFormValidation(id :any) {
- 
-          this.personForm = this.fb.group({ 
-                'empId': [id],
-                'empName': ['', [Validators.required, Validators.minLength(1)]],
-                'empIdno': ['', [Validators.required, Validators.minLength(1)]], 
-                'empAd1': ['', [Validators.required, Validators.minLength(1)]],
-                'empAd2': ['', [Validators.required, Validators.minLength(1)]],
-                'empAd3': ['', [Validators.required, Validators.minLength(1)]],
-                'jobTitleId': ['', [Validators.required, Validators.minLength(1), Validators.min(1)]]
-            }); 
-        
+@Component({
+  selector: 'app-employee-component',
+  templateUrl: './employee-component.component.html',
+  styleUrls: ['./employee-component.component.css']
+})
+
+export class EmployeeComponentComponent implements OnInit {
+
+  private person: EmployeeModel = new EmployeeModel();
+  private personForm: FormGroup;
+  private intention: number = UPDATE;
+
+  display: boolean = false;
+
+  formTitle: string = "New Employee";
+  dataList: Array<any> = new Array<any>();
+  jobTitleDataList: Array<any> = new Array<any>();
+
+  formErrors = {
+    'empName': '',
+    'empIdno': '',
+    'empAd1': '',
+    'empAd2': '',
+    'empAd3': '',
+    'jobTitleId': ''
+  };
+
+  itemSelected: boolean = false;
+
+  validationMessages = {
+    'empName': {
+      'required': 'Name is required.'
+    },
+    'empIdno': {
+      'required': 'IC No. is required.'
+    },
+
+    'empAd1': {
+      'required': 'Address 1 is required.'
+    },
+    'empAd2': {
+      'required': 'Address 2 is required.'
+    },
+    'empAd3': {
+      'required': 'Address3 is required.'
+    },
+    'jobTitleId': {
+      'required': 'Job Title is required.'
+    }
+  };
+
+  userSubscription: Subscription;
+
+  rows = [];
+  jobTitleRows = [];
+
+  columns = [
+    { prop: 'empName', name: 'Name' },
+    { prop: 'empIdno', name: 'Employee No' },
+    { prop: 'empAd1', name: 'Address', width: 350 },
+    { prop: 'empAd2', name: 'Address 2', width: 350 },
+    { prop: 'empAd3', name: 'Address 3', width: 350 }
+  ];
+
+  constructor(private store: Store<CityAppState>, private fb: FormBuilder) { }
+
+  name: string;
+  description: string;
+
+  ngOnInit() {
+
+    this.userSubscription = this.store.subscribe(appData => {
+
+      this.componentMessageHandle(messageUtil.getMultiMessage(appData,
+        [EMPLOYEE_GET_OK, EMPLOYEE_SAVE_SUCCESS, JOBTITLE_GET_OK]));
+    });
+
+    this.configureEditForm();
+  }
+
+  ngAfterViewInit() {
+    this.dispatchIntent(EMPLOYEE_GET);
+    this.dispatchIntent(JOBTITLE_GET);
+  }
+
+  save() {
+
+    var saveJson = new EmployeeModel();
+    if (this.intention == ADD) {
+      saveJson = this.personForm.value as EmployeeModel;
+    }
+    else {
+
+      saveJson.empId = this.person.empId;
+      saveJson.empName = this.person.empName;
+      saveJson.empIdno = this.person.empIdno;
+      saveJson.empAd1 = this.person.empAd1;
+      saveJson.empAd2 = this.person.empAd2;
+      saveJson.empAd3 = this.person.empAd3;
+      saveJson.jobTitleId = this.person.jobTitleId;
+    }
+
+    var strJson = JSON.stringify(saveJson);
+    this.dispatchIntent(EMPLOYEE_SAVE, saveJson);
+    this.display = false;
+  }
+
+  private setFormValidation(id: any) {
+
+    this.personForm = this.fb.group({
+      'empId': [id],
+      'empName': ['', [Validators.required, Validators.minLength(1)]],
+      'empIdno': ['', [Validators.required, Validators.minLength(1)]],
+      'empAd1': ['', [Validators.required, Validators.minLength(1)]],
+      'empAd2': ['', [Validators.required, Validators.minLength(1)]],
+      'empAd3': ['', [Validators.required, Validators.minLength(1)]],
+      'jobTitleId': ['', [Validators.required, Validators.minLength(1), Validators.min(1)]]
+    });
+
+  }
+
+  private configureAddForm() {
+    this.setFormValidation('');
+
+    for (const field in this.formErrors) {
+      this.formErrors[field] = '';
+    }
+  }
+
+  private configureEditForm() {
+
+    this.setFormValidation(this.person.empId);
+    this.personForm.valueChanges.debounceTime(300)
+      .subscribe(data => this.onValueChanged(data));
+  }
+
+  onValueChanged(data?: EmployeeModel) {
+
+    if (!this.personForm) { return; }
+
+    const form = this.personForm;
+    this.person.empId = data.empId;
+    this.person.empName = data.empName;
+    this.person.empIdno = data.empIdno;
+    this.person.empAd1 = data.empAd1;
+    this.person.empAd2 = data.empAd2;
+    this.person.empAd3 = data.empAd3;
+    this.person.jobTitleId = data.jobTitleId;
+
+    for (const field in this.formErrors) {
+      // clear previous error message (if any)
+      this.formErrors[field] = '';
+      const control = form.get(field);
+
+      if (control && control.dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+        for (const key in control.errors) {
+          this.formErrors[field] += messages[key] + ' ';
+        }
       }
-            
-      private configureAddForm()
-      {
-        this.setFormValidation(''); 
- 
-        for (const field in this.formErrors) { 
-          this.formErrors[field] = ''; 
-        } 
+    }
+  }
+
+  componentMessageHandle(messageAll: Array<any>) {
+
+    messageAll.map(message => {
+
+      if (message && message.type == EMPLOYEE_GET_OK) {
+        this.rows.length = 0;
+        for (var userInfo of message.data.data.data) {
+          this.dataList.push({
+            empId: userInfo.empId,
+            empName: userInfo.empName,
+            empIdno: userInfo.empIdno,
+            empAd1: userInfo.empAd1,
+            empAd2: userInfo.empAd2,
+            empAd3: userInfo.empAd3,
+            jobTitleId: userInfo.jobTitleId
+          });
+        }
+        this.rows = this.dataList;
       }
-                
-      private configureEditForm() { 
-       
-        this.setFormValidation(this.person.empId); 
-        this.personForm.valueChanges.debounceTime(300)
-        .subscribe(data => this.onValueChanged(data));
-     }
 
-        onValueChanged(data?: EmployeeModel) {
-          
-          if (!this.personForm) { return; }              
-          
-          const form = this.personForm;
-          this.person.empId = data.empId;
-          this.person.empName = data.empName;
-          this.person.empIdno = data.empIdno;              
-          this.person.empAd1 = data.empAd1;
-          this.person.empAd2 = data.empAd2;
-          this.person.empAd3 = data.empAd3;
-          this.person.jobTitleId = data.jobTitleId;
-          
-          for (const field in this.formErrors) {
-            // clear previous error message (if any)
-            this.formErrors[field] = '';
-            const control = form.get(field);
-            
-            if (control && control.dirty && !control.valid) {
-              const messages = this.validationMessages[field];
-              for (const key in control.errors) {
-                this.formErrors[field] += messages[key] + ' ';
-              }
-            }
-          }   
-        }
-                          
-        componentMessageHandle(messageAll : Array<any>) {
-          
-          messageAll.map(message => {  
-            
-            if (message && message.type == EMPLOYEE_GET_OK)
-            {
-              this.rows.length = 0;
-              for (var userInfo of message.data.data.data)
-              {                    
-                this.dataList.push({  
-                  empId : userInfo.empId,
-                  empName : userInfo.empName, 
-                  empIdno : userInfo.empIdno,           
-                  empAd1 : userInfo.empAd1,
-                  empAd2 : userInfo.empAd2,
-                  empAd3 : userInfo.empAd3,
-                  jobTitleId : userInfo.jobTitleId
-                });
-              }                
-              this.rows = this.dataList;
-            }    
-            
-            if (message && message.type == EMPLOYEE_SAVE_SUCCESS)
-            {                  
-              this.display = false;                
-            }    
+      if (message && message.type == EMPLOYEE_SAVE_SUCCESS) {
+        this.display = false;
+      }
 
-             if (message && message.type == JOBTITLE_GET_OK)
-            {
-              this.jobTitleRows.length = 0;
-              for (var d of message.data.data.data)
-              {   
-                console.log(d);
-                this.jobTitleDataList.push({   
-                    jobTitleId : d.jobTitleId,
-                    jobTitleName : d.jobTitleName 
-                });
-              }
+      if (message && message.type == JOBTITLE_GET_OK) {
+        this.jobTitleRows.length = 0;
+        for (var d of message.data.data.data) {
+          console.log(d);
+          this.jobTitleDataList.push({
+            jobTitleId: d.jobTitleId,
+            jobTitleName: d.jobTitleName
+          });
+        }
 
-              this.jobTitleRows = this.jobTitleDataList;
-            } 
-          });                
-        }
-        
-        onSelect(evt : any) {
-          
-          if (evt && evt.selected && evt.selected.length > 0)
-          {
-            this.person = evt.selected[0] as EmployeeModel;                   
-            this.itemSelected = true;   
-          }
-          else 
-          this.itemSelected = false;
-          
-            this.edit();
-        }
-        
-        addForm() {        
-          
-          this.formTitle = "New Employee"; 
-          this.display = true;                          
-          this.intention = ADD;
-          this.configureAddForm();  
-        }   
-        
-        edit() {  
-          
-          this.formTitle = "Edit Employee"; 
-          this.intention = UPDATE;                            
-          this.configureEditForm();
-          
-          if (this.person)
-          {                                 
-            this.personForm.get("empName").setValue(this.person.empName);
-            this.personForm.get("empIdno").setValue(this.person.empIdno);
-            this.personForm.get("empId").setValue(this.person.empId);
-            this.personForm.get("empAd1").setValue(this.person.empAd1);
-            this.personForm.get("empAd2").setValue(this.person.empAd2);
-            this.personForm.get("empAd3").setValue(this.person.empAd3);  
-            this.personForm.get("jobTitleId").setValue(this.person.jobTitleId);                 
-            this.display = true;
-          }       
-        }                          
-        
-        cancel() 
-        {
-          this.display = false;     
-          this.itemSelected = false;          
-        }                          
-        
-        dispatchIntent(messageType : string, data? : any)
-        {   
-          this.store.dispatch(
-            {     
-              type: messageType,
-              data : data
-            });      
-          }  
-        }
-        
+        this.jobTitleRows = this.jobTitleDataList;
+      }
+    });
+  }
+
+  onSelect(evt: any) {
+
+    if (evt && evt.selected && evt.selected.length > 0) {
+      this.person = evt.selected[0] as EmployeeModel;
+      this.itemSelected = true;
+    }
+    else
+      this.itemSelected = false;
+
+    this.edit();
+  }
+
+  addForm() {
+
+    this.formTitle = "New Employee";
+    this.display = true;
+    this.intention = ADD;
+    this.configureAddForm();
+  }
+
+  edit() {
+
+    this.formTitle = "Edit Employee";
+    this.intention = UPDATE;
+    this.configureEditForm();
+
+    if (this.person) {
+      this.personForm.get("empName").setValue(this.person.empName);
+      this.personForm.get("empIdno").setValue(this.person.empIdno);
+      this.personForm.get("empId").setValue(this.person.empId);
+      this.personForm.get("empAd1").setValue(this.person.empAd1);
+      this.personForm.get("empAd2").setValue(this.person.empAd2);
+      this.personForm.get("empAd3").setValue(this.person.empAd3);
+      this.personForm.get("jobTitleId").setValue(this.person.jobTitleId);
+      this.display = true;
+    }
+  }
+
+  cancel() {
+    this.display = false;
+    this.itemSelected = false;
+  }
+
+  dispatchIntent(messageType: string, data?: any) {
+    messageUtil.dispatchIntent(this.store, messageType, data);
+  }
+
+}
--- WebUx/src/app/employee/employee-component/employeeEffects.ts
+++ WebUx/src/app/employee/employee-component/employeeEffects.ts
@@ -5,90 +5,94 @@ import { Observable } from 'rxjs/Observable';
 import { Store } from '@ngrx/store';
 import * as messageUtil from "../../sharedObjects/storeMessageUtil";
 
-import {EMPLOYEE_SAVE, EMPLOYEE_CANCEL, EMPLOYEE_SAVE_SUCCESS,
-  EMPLOYEE_MESSAGE_END, EMPLOYEE_SAVE_ERR, EMPLOYEE_CANCEL_OK, EMPLOYEE_WAIT_PENDING, EMPLOYEE_GET, EMPLOYEE_GET_ERR,
-  EMPLOYEE_GET_OK, JOBTITLE_GET, JOBTITLE_GET_OK,  CityAppState, CityData, headersJson } from '../../sharedObjects/sharedMessages';
-  import { APPLICATION_HOST } from '../../sharedObjects/applicationSetup';
-  import 'rxjs/Rx';
-  import { subscribeOn } from 'rxjs/operator/subscribeOn';
-  
-  @Injectable()
-  export class EmployeeEffects {   
-    
-    constructor(
-      private http: HttpClient,
-      private actions$: Actions<CityAppState>, private store : Store<CityAppState>,
-    ) { }
-    
-    @Effect() citySave$ = this.actions$    
+import {
+  EMPLOYEE_SAVE, EMPLOYEE_CANCEL, EMPLOYEE_SAVE_SUCCESS,
+  EMPLOYEE_MESSAGE_END, EMPLOYEE_SAVE_ERR, EMPLOYEE_CANCEL_OK, EMPLOYEE_WAIT_PENDING,
+  EMPLOYEE_GET, EMPLOYEE_GET_ERR,
+  PROGRESS_WAIT_SHOW, PROGRESS_WAIT_HIDE,
+  EMPLOYEE_GET_OK, JOBTITLE_GET, JOBTITLE_GET_OK, CityAppState, CityData, headersJson
+} from '../../sharedObjects/sharedMessages';
+import { APPLICATION_HOST } from '../../sharedObjects/applicationSetup';
+import 'rxjs/Rx';
+import { subscribeOn } from 'rxjs/operator/subscribeOn';
+
+@Injectable()
+export class EmployeeEffects {
+
+  constructor(
+    private http: HttpClient,
+    private actions$: Actions<CityAppState>, private store: Store<CityAppState>,
+  ) { }
+
+  @Effect() citySave$ = this.actions$
     .ofType(EMPLOYEE_SAVE)
-    .map(action => {  
-      console.log("save action",action);
+    .map(action => {
+      console.log("save action", action);
       return JSON.stringify(action.data);
-    }).switchMap(payload =>   
-    {
-          /////////EXTRA CODE /////////////////////////////
-  
-          return Observable.of(payload)
-          .map(action => {
-            
-            this.http.post(APPLICATION_HOST + '/employee/save', payload, {headers : headersJson})
-            .subscribe(res => {                   
-              messageUtil.dispatchIntent(this.store, EMPLOYEE_SAVE_ERR, null);
-            },  
-            err => {                       
-              if (err && err.status == 201)
-              {
-                messageUtil.dispatchIntent(this.store, EMPLOYEE_SAVE_SUCCESS, null);                
-              } 
-              else 
-              {                    
-                 messageUtil.dispatchIntent(this.store, EMPLOYEE_SAVE_ERR, null);             
-              }         
-            });  
-          });          
-        
-          ///////////EXTRA CODE ENDS ///////////////////////////          
+    }).switchMap(payload => {
+      /////////EXTRA CODE /////////////////////////////
+
+      return Observable.of(payload)
+        .map(action => {
+
+          this.http.post(APPLICATION_HOST + '/employee/save', payload, { headers: headersJson })
+            .subscribe(res => {
+              messageUtil.dispatchIntent(this.store, EMPLOYEE_SAVE_SUCCESS, null);
+              messageUtil.dispatchIntent(this.store, PROGRESS_WAIT_HIDE, null);
+
+            },
+            err => {
+              if (err && err.status == 201) {
+                messageUtil.dispatchIntent(this.store, EMPLOYEE_SAVE_SUCCESS, null);
+                messageUtil.dispatchIntent(this.store, PROGRESS_WAIT_HIDE, null);
+
+              }
+              else {
+                messageUtil.dispatchIntent(this.store, EMPLOYEE_SAVE_ERR, null);
+                messageUtil.dispatchIntent(this.store, PROGRESS_WAIT_HIDE, null);
+              }
+            });
+        });
+
+      ///////////EXTRA CODE ENDS ///////////////////////////          
+    })
+    .concatMap(res => {
+      return Observable.of({ type: PROGRESS_WAIT_SHOW });
+    });
+
+
+  @Effect() cityReset$ = this.actions$
+    .ofType(EMPLOYEE_CANCEL)
+    .map(action => {
+      return ({ type: EMPLOYEE_CANCEL_OK });
+    });
+
+  @Effect() cityGet$ = this.actions$
+    .ofType(EMPLOYEE_GET)
+    .map(action => {
+      JSON.stringify(action);
+    })
+    .switchMap(
+
+    payload => this.http.get(APPLICATION_HOST + '/employee/index').map(res => {
+      return { type: EMPLOYEE_GET_OK, data: res };
+    }).catch(() => Observable.of({ type: EMPLOYEE_GET_ERR }))
+
+    );
+
+  @Effect() GrnGetJobTitle$ = this.actions$
+    .ofType(JOBTITLE_GET)
+    .map(action => {
+      JSON.stringify(action);
+    })
+    .switchMap(payload =>
+
+      this.http.get(APPLICATION_HOST + '/JobTitle/index')
+        .map(res => {
+          return { type: JOBTITLE_GET_OK, data: res };
         })
-        .concatMap(res => {         
-          return Observable.of({ type: EMPLOYEE_WAIT_PENDING });
-        });       
-    
-    
-    @Effect() cityReset$ = this.actions$  
-    .ofType(EMPLOYEE_CANCEL)  
-    .map(action => 
-    {
-        return ({ type: EMPLOYEE_CANCEL_OK});
-    }); 
-                  
-    @Effect() cityGet$ = this.actions$    
-      .ofType(EMPLOYEE_GET)     
-      .map(action => {   
-        JSON.stringify(action);
-      })
-      .switchMap(
-        
-            payload => this.http.get(APPLICATION_HOST + '/employee/index').map(res => {       
-              return { type: EMPLOYEE_GET_OK, data: res};
-            }).catch(() => Observable.of({ type: EMPLOYEE_GET_ERR }))
-            
-      ); 
-      
-      @Effect() GrnGetJobTitle$ = this.actions$    
-      .ofType(JOBTITLE_GET)     
-      .map(action => {   
-        JSON.stringify(action);
-      })
-      .switchMap(payload => 
-        
-          this.http.get(APPLICATION_HOST + '/JobTitle/index')  
-          .map(res => {       
-            return { type: JOBTITLE_GET_OK, data: res};
-          }) 
-          .catch(() => Observable.of({ type: EMPLOYEE_GET_ERR }))
-    ); 
-    
-  }
-  
-  
+        .catch(() => Observable.of({ type: EMPLOYEE_GET_ERR }))
+    );
+
+}
+
