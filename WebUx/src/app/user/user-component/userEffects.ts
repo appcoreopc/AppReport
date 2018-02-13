b3381ecfbfd978d6890e64f20c98d3c97e5f87a8
kepung <kepung@gmail.com>
2018-02-13 19:48:07 +1300

    syn user form with other form.

--- WebUx/src/app/user/user-component/userEffects.ts
+++ WebUx/src/app/user/user-component/userEffects.ts
@@ -1,36 +1,61 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Actions, Effect } from '@ngrx/effects';
+import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
 import {USER_SAVE, USER_CANCEL, USER_SAVE_SUCCESS,
-  USER_MESSAGE_END, USER_SAVE_ERR, USER_CANCEL_OK, USER_GET, USER_GET_ERR,
+  USER_MESSAGE_END, USER_SAVE_ERR, USER_CANCEL_OK, USER_GET, USER_GET_ERR, PROGRESS_WAIT_SHOW, PROGRESS_WAIT_HIDE,
   USER_GET_OK, CityAppState, CityData, headersJson } from '../../sharedObjects/sharedMessages';
   import { APPLICATION_HOST, APP_SERVICE_PATH} from '../../sharedObjects/applicationSetup';
   import 'rxjs/Rx';
-  
+import * as messageUtil from "../../sharedObjects/storeMessageUtil";
+
   @Injectable()  
   export class UserEffects {   
     
     constructor(
       private http: HttpClient,
-      private actions$: Actions<CityAppState>
+      private actions$: Actions<CityAppState>,
+      private store: Store<CityAppState>,
     ) { 
-
-     }
+      
+    }
     
     @Effect() userSave$ = this.actions$    
     .ofType(USER_SAVE)   
     .map(action => {  
-     return JSON.stringify(action.data);   
+      return JSON.stringify(action.data);   
     })
     .switchMap(payload =>    {
-     console.log('posting');
-     console.log(payload);
-     return this.http.post(APPLICATION_HOST + '/user/save', payload, {headers : headersJson});
+      
+      return Observable.of(payload)
+        .map(action => {
+
+          this.http.post(APPLICATION_HOST + '/user/save', payload, {headers : headersJson})
+            .subscribe(res => {
+              messageUtil.dispatchIntent(this.store, USER_SAVE_SUCCESS, null);
+              messageUtil.dispatchIntent(this.store, PROGRESS_WAIT_HIDE, null);
+
+            },
+            err => {
+              if (err && err.status == 201) {
+                messageUtil.dispatchIntent(this.store, USER_SAVE_SUCCESS, null);
+                messageUtil.dispatchIntent(this.store, PROGRESS_WAIT_HIDE, null);
+
+              }
+              else {
+                messageUtil.dispatchIntent(this.store, USER_SAVE_SUCCESS, null);
+                messageUtil.dispatchIntent(this.store, PROGRESS_WAIT_HIDE, null);
+              }
+            });
+        });
+       
     })
-    .map(res => ({ type: USER_SAVE_SUCCESS, data: res }))
-    .catch(() => Observable.of({ type: USER_SAVE_ERR }));
-            
+    .concatMap(res => {
+      return Observable.of({ type: PROGRESS_WAIT_SHOW });
+    });
+    
+    
     @Effect() userReset$ = this.actions$  
     .ofType(USER_CANCEL)  
     .map(action => 
@@ -38,16 +63,16 @@ import {USER_SAVE, USER_CANCEL, USER_SAVE_SUCCESS,
         return ({ type: USER_CANCEL_OK});
       }); 
       
-  @Effect() userGet$ = this.actions$    
+      @Effect() userGet$ = this.actions$    
       .ofType(USER_GET)     
       .switchMap(payload => this.http.get(APPLICATION_HOST + '/user/index')  
       .map(res => {   
-                
+        
         return { type: USER_GET_OK, data: res};
       }) 
       .catch(() => Observable.of({ type: USER_GET_ERR }))
     ); 
-
+    
   }
   
   
