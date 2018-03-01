2e7a6f64a8be5571b3345d4fc0322922d218c3c2
kepung <kepung@gmail.com>
2018-03-01 21:03:45 +1300

    adding a global time delay

--- WebUx/src/app/sharedObjects/applicationSetup.ts
+++ WebUx/src/app/sharedObjects/applicationSetup.ts
@@ -1,3 +1,4 @@
 export const APPLICATION_HOST  = 'http://localhost:5050';
 export const APP_SERVICE_PATH = '';
 export const APPLICATION_HOST2 = 'http://localhost:5050';
+export const TIME_DELAY : number = 3000;
