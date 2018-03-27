a38bd3581a27d00cf23754650062e8a86e3eb3f9
jeremy_woo <jeremy.woo@clearpoint.co.nz>
2018-03-28 07:51:58 +1300

    added health controller.

--- AppReport/Controllers/HealthController.cs
+++ AppReport/Controllers/HealthController.cs
@@ -1,15 +1,54 @@
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using AppReport.DataServices.PTSDataModel;
+using AppReport.Config;
 
-namespace AppReport.Controllers
+namespace ACC.IDS.NetCore.Service.SelfHost.Controllers
 {
+    [Route("api/[controller]")]
     public class HealthController : Controller
     {
+        private PTSContext _ptsContext;
+        private AppConfig _accessConfig;
 
-        [HttpOptions]
-        public IActionResult Index()
+        public HealthController(PTSContext ptsContext, IOptions<AppConfig> accessConfig)
         {
-            // 
-            return Ok("POST,GET,PUT,DELETE");
+            _ptsContext = ptsContext;
+            _accessConfig = accessConfig?.Value;
         }
+        
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {                    
+             return TestDatabaseConnection();         
+        }
+                       
+        private string[] TestDatabaseConnection()
+        {
+            try
+            {
+                var conn = _ptsContext.Database.GetDbConnection();
+
+                if (conn.State == System.Data.ConnectionState.Closed)
+                    conn.Open();
+                
+                return new string[] {
+                    "DatabaseConnection",  $"{_accessConfig?.ConnectionStrings.PTSDatabase}",
+                    "ConnectionState: " , conn.State.ToString(), "PTS File Path" , _accessConfig.ConnectionStrings.ReportFilePath
+                };
+            }
+            catch (Exception ex)
+            {
+                return new string[] {
+                    "DatabaseConnection",  "Error connecting to database."
+                };
+            }
+        }     
+      
     }
 }
