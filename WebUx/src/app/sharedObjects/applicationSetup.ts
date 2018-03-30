6fd79679dcef04fd41284d7ef36f8a0206c12192
kepung <kepung@gmail.com>
2018-03-30 19:09:58 +1300

    appsettings changes

--- WebUx/src/app/sharedObjects/applicationSetup.ts
+++ WebUx/src/app/sharedObjects/applicationSetup.ts
@@ -1,4 +1,4 @@
-export const APPLICATION_HOST  = 'http://localhost:5050';
+export const APPLICATION_HOST  = 'http://192.168.1.18:5050';
 export const APP_SERVICE_PATH = '';
-export const APPLICATION_HOST2 = 'http://localhost:5050';
+export const APPLICATION_HOST2 = 'http://192.168.1.18:5050';
 export const TIME_DELAY : number = 3000;
