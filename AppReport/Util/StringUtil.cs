bacf599efaf9dcd15271112ab1a5a49347d17eb6
jeremy_woo <jeremy.woo@clearpoint.co.nz>
2018-04-10 10:17:57 +1200

    added stringutil

---
+++ AppReport/Util/StringUtil.cs
@@ -0,0 +1,29 @@
+﻿
+
+using System;
+using System.Linq;
+
+namespace AppReport.Util
+{
+    public static class StringUtil
+    {  
+        public static string[] SplitByComma(this string source)
+        {
+            if (!string.IsNullOrEmpty(source))
+            {
+                return source.Split(',');
+            }
+            return null;
+        }
+
+        public static int[] ToIntList(this string[] source)
+        {
+            if (source != null 
+                &&  source.Length > 0)
+            {
+                return source.Select(x => Convert.ToInt32(x))?.ToArray();
+            }
+            return null;
+        }
+    }
+}
