a00643a21348329a2b9312d679df80c314bfbc34
kepung <kepung@gmail.com>
2018-04-09 20:32:44 +1200

    ready stock delete with reloading issues.

--- WebUx/src/app/material/readystock-component/readyStockReducer.ts
+++ WebUx/src/app/material/readystock-component/readyStockReducer.ts
@@ -1,8 +1,8 @@
 import { ActionReducer, Action } from '@ngrx/store';
 
-import {READYSTOCK_SAVE, READYSTOCK_CANCEL, READYSTOCK_SAVE_SUCCESS,
+import {READYSTOCK_SAVE, READYSTOCK_CANCEL, READYSTOCK_SAVE_SUCCESS, 
 	 READYSTOCK_MESSAGE_END, READYSTOCK_SAVE_ERR, READYSTOCK_CANCEL_OK, READYSTOCK_GET, READYSTOCK_GET_ERR,
-	  READYSTOCK_GET_OK, READYSTOCK_WAIT_PENDING, CityAppState, CityData } from '../../sharedObjects/sharedMessages';
+	  READYSTOCK_GET_OK, READYSTOCK_WAIT_PENDING, CityAppState, CityData, READYSTOCK_DELETE_SUCCESS } from '../../sharedObjects/sharedMessages';
 
 		export function ReadyStockReducer(status: CityAppState, action: Action) {
 	switch (action.type) {
@@ -11,17 +11,15 @@ import {READYSTOCK_SAVE, READYSTOCK_CANCEL, READYSTOCK_SAVE_SUCCESS,
 		case READYSTOCK_SAVE:			 
 		  return  { status : 2, data : action, type: READYSTOCK_SAVE };	
 		case READYSTOCK_CANCEL:
-			console.log(READYSTOCK_CANCEL);
 			return  { status : 3, type: READYSTOCK_MESSAGE_END };	
 		case READYSTOCK_SAVE_SUCCESS:
-			console.log(READYSTOCK_SAVE_SUCCESS);
 			return { status : 4, type: READYSTOCK_SAVE_SUCCESS }
 		case READYSTOCK_SAVE_ERR:
-			console.log(READYSTOCK_SAVE_ERR);
 			return  { status : 5, type: READYSTOCK_MESSAGE_END };		
 		case READYSTOCK_WAIT_PENDING:	
-		  console.log('READYSTOCK pending');	  
 			return  { status : 6, data : action, type: READYSTOCK_WAIT_PENDING };	 
+		case READYSTOCK_DELETE_SUCCESS:	
+			return  { status : 7, data : action, type: READYSTOCK_DELETE_SUCCESS };	 
 		default:
 			return status;						
 		}					
--- WebUx/src/app/material/readystock-component/readystock-component.component.ts
+++ WebUx/src/app/material/readystock-component/readystock-component.component.ts
@@ -6,9 +6,9 @@ import * as timeUtil from '../../sharedObjects/timeUtil';
 import { TIME_DELAY } from '../../sharedObjects/applicationSetup';
 
 import { CityAppState, READYSTOCK_GET, 
-  READYSTOCK_GET_OK, READYSTOCK_SAVE, 
-  READYSTOCK_SAVE_SUCCESS, READYSTOCK_SAVE_ERR,
-  PROGRESS_WAIT_SHOW, PROGRESS_WAIT_HIDE,
+  READYSTOCK_GET_OK, READYSTOCK_SAVE, READYSTOCK_DELETE_SUCCESS,
+  READYSTOCK_SAVE_SUCCESS, READYSTOCK_SAVE_ERR,DELETE_ITEM_DELIMITER, 
+  PROGRESS_WAIT_SHOW, PROGRESS_WAIT_HIDE,READYSTOCK_DELETE, 
   UOM_CANCEL, UOM_CANCEL_OK, UOM_GET, UOM_GET_ERR, UOM_GET_OK, ADD, UPDATE
 } from '../../sharedObjects/sharedMessages';
 
@@ -28,6 +28,10 @@ export class ReadyStockComponent implements OnInit {
   personForm: FormGroup;
   private intention : number = UPDATE;
   formUtil : FormUtil<ReadyStockModel>;
+  isTargetCheckbox : boolean = false;
+  selected : any;
+
+  isLoading : boolean = false;
   
   formValidators = {   
     'readyStockId': [],
@@ -87,174 +91,223 @@ export class ReadyStockComponent implements OnInit {
       this.userSubscription = this.store.subscribe(appData => {     
         
         this.componentMessageHandle(messageUtil.getMultiMessage(appData, 
-          [READYSTOCK_GET_OK, READYSTOCK_SAVE_SUCCESS]));    
-        }); 
-        
-        this.configureUpdateForm();      
-      }
-      
-      ngAfterViewInit() {
-        
-        this.getReadyStock();
-      }
-      
-      componentMessageHandle(messageAll : Array<any>) {
-        
-        messageAll.map(async message => { 
+          [READYSTOCK_GET_OK, READYSTOCK_SAVE_SUCCESS, 
+            READYSTOCK_DELETE_SUCCESS]));    
+          }); 
           
-          if (message && message.type == READYSTOCK_GET_OK)
-          {
-            this.rows.length = 0;
-            this.dataList.length = 0;
+          this.configureUpdateForm();      
+        }
+        
+        ngAfterViewInit() {
+          
+          this.getReadyStock();
+        }
+        
+        componentMessageHandle(messageAll : Array<any>) {
+          
+          messageAll.map(async message => { 
             
-            for (var rs of message.data.data.data)
-            {             
-              let model = new ReadyStockModel();
-              model = {...rs};
-              this.dataList.push(model);  
+            if (message && message.type == READYSTOCK_GET_OK)
+            {
+              this.rows.length = 0;
+              this.dataList.length = 0;
+              
+              for (var rs of message.data.data.data)
+              {             
+                let model = new ReadyStockModel();
+                model = {...rs};
+                this.dataList.push(model);  
+              }
+              this.rows = this.dataList;
             }
-            this.rows = this.dataList;
-          }
-          
-          if (message && message.type == READYSTOCK_SAVE_SUCCESS)
-          {
-            await timeUtil.delay(TIME_DELAY);        
-            this.getReadyStock();
-            this.display = false;             
-          }
-        });    
-      }
-      
-      save() {     
+            
+            if (message && (message.type == READYSTOCK_SAVE_SUCCESS || message.type == READYSTOCK_DELETE_SUCCESS))
+            {
+              if (this.isLoading == false)
+              {
+                await timeUtil.delay(TIME_DELAY);        
+                this.getReadyStock();
+                this.display = false;             
+                this.isLoading = true;
+              }
+            }
+          });    
+        }
         
-        debugger;
-        
-        let mainFormModel = this.formUtil.commit();
-        
-        if (this.intention == ADD)
-        {
-          mainFormModel.readyStockId = null;
-        }      
-        
-        this.person.readyStockDesc = mainFormModel.readyStockDesc;
-        this.person.tariffCode = mainFormModel.tariffCode; 
-        this.person.dutyImpRate = mainFormModel.dutyImpRate;
-        this.person.gstrate = mainFormModel.gstrate;
-                                       
-        this.rows = [...this.rows];
+        save() {     
 
-        this.dispatchIntent(READYSTOCK_SAVE, mainFormModel);
-        this.display = false; 
-      }  
-      
-      private configureAddForm() {
-        
-        this.person = new ReadyStockModel();
-        this.person.readyStockId = 0; 
-        this.person.readyStockDesc = '';
-        this.person.tariffCode = ''; 
-        this.person.dutyImpRate = 0;
-        this.person.gstrate = 0;
-        
-        this.formUtil = new FormUtil<ReadyStockModel>(this.person, this.formValidators);
-        let userform = this.formUtil.createForm(false);
-        this.personForm = userform;
-        
-        this.personForm.valueChanges.debounceTime(500)
-        .subscribe(data => this.onValueChanged(data));
-        
-      }
-      
-      private configureUpdateForm() {
-        this.personForm = this.fb.group({
-          'readyStockId': [this.rsModel.readyStockId],
-          'readyStockDesc': [this.rsModel.readyStockDesc, [Validators.required, Validators.minLength(1)]],
-          'tariffCode': [this.rsModel.tariffCode, [Validators.required, Validators.minLength(1)]],
-          'dutyImpRate': [this.rsModel.dutyImpRate, [Validators.required, Validators.minLength(1)]],
-          'gstrate': [this.rsModel.gstrate, [Validators.required, Validators.minLength(1)]]
-        });
-        
-        this.personForm.valueChanges.debounceTime(500)
-        .subscribe(data => this.onValueChanged(data));
-      }
-      
-      onValueChanged(data?: ReadyStockModel) {
-        
-        if (!this.personForm) { return; }
-        
-        const form = this.personForm;    
-        
-        for (const field in this.formErrors) {
-          // clear previous error message (if any)
-          this.formErrors[field] = '';
-          const control = form.get(field);
+          this.isLoading = false;
           
-          if (control && control.dirty && !control.valid) {
-            const messages = this.validationMessages[field];
-            for (const key in control.errors) {
-              this.formErrors[field] += messages[key] + ' ';
-            }
-          }
-        }   
-      }
-      
-      dispatchIntent(messageType : string, data? : any)
-      {   
-        this.store.dispatch(
-          {     
-            type: messageType,
-            data : data
-          });      
-        }          
-        
-        addForm() {              
+          let mainFormModel = this.formUtil.commit();
           
-          this.formTitle = "New Ready Stock"; 
-          this.display = true;                          
-          this.intention = ADD;
-          this.configureAddForm();  
-        }            
-        
-        onSelect(evt : any) {
-          
-          if (evt && evt.selected && evt.selected.length > 0)
+          if (this.intention == ADD)
           {
-            
-            this.rsModel = evt.selected[0] as ReadyStockModel; 
-            this.itemSelected = true;   
-            
-            this.formUtil = new FormUtil<ReadyStockModel>(this.rsModel, 
-              this.formValidators);
-              
-              let form = this.formUtil.createForm(false);
-              this.personForm = form;
-              
-              this.personForm.valueChanges.debounceTime(300)
-              .subscribe(data => this.onValueChanged(data));
-              
-            } 
-            else 
-            this.itemSelected = false;
-            
-            this.edit();
-          }          
+            mainFormModel.readyStockId = null;
+          }      
           
-          edit() {  
-            this.formTitle = "Edit Ready Stock"; 
-            this.intention = UPDATE;
-            this.display = true;                          
-          }
+          this.person.readyStockDesc = mainFormModel.readyStockDesc;
+          this.person.tariffCode = mainFormModel.tariffCode; 
+          this.person.dutyImpRate = mainFormModel.dutyImpRate;
+          this.person.gstrate = mainFormModel.gstrate;
           
-          cancel() 
-          {                               
-            this.person = this.formUtil.rollback();
-            this.itemSelected = false;
-            this.display = false;       
-          }
+          this.rows = [...this.rows];
           
-          getReadyStock(): void {   
-            this.dispatchIntent(READYSTOCK_GET);
-          }
+          this.dispatchIntent(READYSTOCK_SAVE, mainFormModel);
+          this.display = false; 
+        }  
+        
+        private configureAddForm() {
+          
+          this.person = new ReadyStockModel();
+          this.person.readyStockId = 0; 
+          this.person.readyStockDesc = '';
+          this.person.tariffCode = ''; 
+          this.person.dutyImpRate = 0;
+          this.person.gstrate = 0;
+          
+          this.formUtil = new FormUtil<ReadyStockModel>(this.person, this.formValidators);
+          let userform = this.formUtil.createForm(false);
+          this.personForm = userform;
+          
+          this.personForm.valueChanges.debounceTime(500)
+          .subscribe(data => this.onValueChanged(data));
           
         }
         
+        private configureUpdateForm() {
+          this.personForm = this.fb.group({
+            'readyStockId': [this.rsModel.readyStockId],
+            'readyStockDesc': [this.rsModel.readyStockDesc, [Validators.required, Validators.minLength(1)]],
+            'tariffCode': [this.rsModel.tariffCode, [Validators.required, Validators.minLength(1)]],
+            'dutyImpRate': [this.rsModel.dutyImpRate, [Validators.required, Validators.minLength(1)]],
+            'gstrate': [this.rsModel.gstrate, [Validators.required, Validators.minLength(1)]]
+          });
+          
+          this.personForm.valueChanges.debounceTime(500)
+          .subscribe(data => this.onValueChanged(data));
+        }
+        
+        onValueChanged(data?: ReadyStockModel) {
+          
+          if (!this.personForm) { return; }
+          
+          const form = this.personForm;    
+          
+          for (const field in this.formErrors) {
+            // clear previous error message (if any)
+            this.formErrors[field] = '';
+            const control = form.get(field);
+            
+            if (control && control.dirty && !control.valid) {
+              const messages = this.validationMessages[field];
+              for (const key in control.errors) {
+                this.formErrors[field] += messages[key] + ' ';
+              }
+            }
+          }   
+        }
+        
+        dispatchIntent(messageType : string, data? : any)
+        {   
+          this.store.dispatch(
+            {     
+              type: messageType,
+              data : data
+            });      
+          }          
+          
+          addForm() {              
+            
+            this.formTitle = "New Ready Stock"; 
+            this.display = true;                          
+            this.intention = ADD;
+            this.configureAddForm();  
+          }            
+          
+          editForm(evt : any) {
+            
+            debugger;
+            
+            if (evt && evt.row && evt.row.readyStockId) {
+              
+              let targetDataId = evt.row.readyStockId;
+              
+              if (targetDataId) 
+              {
+                this.rsModel = this.rows.find(x => x.readyStockId == targetDataId);     
+                
+                if (this.rsModel)
+                {                    
+                  this.itemSelected = true;   
+                  
+                  this.formUtil = new FormUtil<ReadyStockModel>(this.rsModel, 
+                    this.formValidators);
+                    
+                    let form = this.formUtil.createForm(false);
+                    this.personForm = form;
+                    
+                    this.personForm.valueChanges.debounceTime(300)
+                    .subscribe(data => this.onValueChanged(data));
+                    
+                  } 
+                  else 
+                  this.itemSelected = false;
+                  
+                  this.edit();
+                }
+              }   
+            }       
+            
+            edit() {  
+              this.formTitle = "Edit Ready Stock"; 
+              this.intention = UPDATE;
+              this.display = true;                          
+            }
+            
+            onActivate(evt) {      
+              
+              if (evt.type && evt.type == 'checkbox')
+              {        
+                this.isTargetCheckbox = true;
+              }
+              else if (evt && evt.type && evt.type == 'click')
+              {
+                if (this.isTargetCheckbox != true)
+                {
+                  this.editForm(evt);
+                }
+                this.isTargetCheckbox = false;
+              }
+            }
+            
+            onSelect(evt: any) {     
+              this.selected = evt.selected;      
+            }
+            
+            deleteForm() 
+            {              
+              this.isLoading = false;
+              
+              if (this.selected && this.selected.length > 0)
+              {       
+                let deleItems = this.selected.map( x  => x.readyStockId);
+                if (deleItems)
+                {
+                  this.dispatchIntent(READYSTOCK_DELETE, { 'deleteItems' : deleItems.join(DELETE_ITEM_DELIMITER)});
+                }
+              }
+            }
+            
+            cancel() 
+            {                               
+              this.person = this.formUtil.rollback();
+              this.itemSelected = false;
+              this.display = false;       
+            }
+            
+            getReadyStock(): void {   
+              this.dispatchIntent(READYSTOCK_GET);
+            }
+            
+          }
+          
